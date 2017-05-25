2125a618bb3f70d799415938c2034bdc9f78c8c9
khai.hoang <khai.hcq@sutrixsolutions.com>
2017-05-25 16:03:01 +0700

    merge

--- CodeChallenge/Pods/Target Support Files/SKPhotoBrowser/SKPhotoBrowser-umbrella.h
+++
@@ -1,9 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-#import "SKPhotoBrowser.h"
-
-FOUNDATION_EXPORT double SKPhotoBrowserVersionNumber;
-FOUNDATION_EXPORT const unsigned char SKPhotoBrowserVersionString[];
-
